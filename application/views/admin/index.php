2234acf7a527873122fd084ad0275531547b543d
AnisaDyah <anisadyah353@gmail.com>
2018-05-17 21:29:55 -0700

    add folder project

---
+++ application/views/admin/index.php
@@ -0,0 +1,23 @@
+
+<?php
+
+$this->load->view('layouts/header');
+
+?>
+
+<div class="col-md-10">
+	<h3>Selamat datang</h3>	
+    <h3>Sistem Informasi Pengelola Jadwal</h3>
+    <h3>Jurusan Teknologi Infromasi</h3>
+    <h3>Politeknik Negeri Malang </h3>
+</div>
+<!-- kalender -->
+<div class="pull-right">
+	<div id="kalender"></div>
+</div>
+
+<?php
+
+$this->load->view('layouts/footer');
+
+?>
