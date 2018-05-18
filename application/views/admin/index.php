327792c81a0c9c83ba015de5a4726e69e760d1e2
AnisaDyah <anisadyah353@gmail.com>
2018-05-17 22:23:19 -0700

    halaman utama untuk admin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -10,6 +10,7 @@ $this->load->view('layouts/header');
     <h3>Sistem Informasi Pengelola Jadwal</h3>
     <h3>Jurusan Teknologi Infromasi</h3>
     <h3>Politeknik Negeri Malang </h3>
+    <h3> by: anisa dyah & eka putri </h3>
 </div>
 <!-- kalender -->
 <div class="pull-right">
