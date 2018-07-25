2a8c2bafb52ef71717158dd7c00e7705a5c78495
unknown <ekaapue@gmail.com>
2018-07-25 09:26:41 -0700

    navbar user

--- application/views/users/layouts/footer.php
+++ application/views/users/layouts/footer.php
@@ -1,5 +1,3 @@
-<footer class="container-fluid text-center">
-  <p> </p>
-</footer>
+</div>
 </body>
 </html>
--- application/views/users/layouts/header.php
+++ application/views/users/layouts/header.php
@@ -1,9 +1,10 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <title>Sistem Informasi Pengelola Jadwal</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Admin JTI</title>
+
    <!-- Core CSS - Include with every page -->
    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.css') ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/js/jquery-ui/jquery-ui.css')?>">
@@ -12,69 +13,65 @@
 	<script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.js')?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/jquery-ui/jquery-ui.js')?>"></script>	
 
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
-    
-    /* Add a gray background color and some padding to the footer */
-    footer {
-      background-color: #f2f2f2;
-      padding: 25px;
-    }
-    
-  .carousel-inner img {
-      width: 100%; /* Set width to 100% */
-      margin: auto;
-      min-height:200px;
-  }
-
-  /* Hide the carousel text when the screen is less than 600 pixels wide */
-  @media (max-width: 600px) {
-    .carousel-caption {
-      display: none; 
-    }
-  }
-  </style>
 </head>
 <body>
+<?php if($this->session->userdata('logged_in')) : ?>
+	<div class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="" class="navbar-brand">Sistem Informasi Pencarian Jadwal </a>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="collapse navbar-collapse">				
+				<ul class="nav navbar-nav navbar-right">
+        <a class="dropdown-toggle" data-toggle="dropdown" role="button" href="#">Hy ,  &nbsp&nbsp<span class="glyphicon glyphicon-user"></span></a></li>
+    </ul>
+			</div>
+		</div>
+	</div>
+  <?php endif; ?>
+  <!-- modal input -->
+  <?php if($this->session->userdata('logged_in')) : ?>
+	<div id="modalpesan" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Pesan Notification</h4>
+				</div>
+				<div class="modal-body">
+				
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>						
+				</div>
+				Mail.com123
+			</div>
+		</div>
+	</div>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="">Pencarian Kelas JTI</a>
+	<div class="col-md-2">
+		<div class="row">
+			<div class="col-xs-6 col-md-12">
+					<a class="thumbnail">
+						<img class="img-responsive" src="<?php echo base_url('assets/logo.png')?>">
+					</a>
+				</div>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="<?php echo site_url('user/index')?>">Home</a></li>
-        <li><a href="<?php echo site_url('user_jadwal/')?>">Jadwal</a></li>
-        <li><a href="<?php echo site_url('user_pencarian/')?>">Pencarian</a></li>      
-      </ul>
-    
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="<?php echo site_url('user/login_form')?>"><span class="glyphicon glyphicon-log-in"></span> Login </a></li>
-      </ul>
-  <ul class="nav navbar-nav navbar-right">
-    <?php if(!$this->session->userdata('logged_in')) : ?>
-        <div class="btn-group" role="group" aria-label="Data baru">            
-        <li> <?php echo anchor('user/login', 'Login', array('class' => 'glyphicon glyphicon-log-in')); ?></li>
-        </div>
     <?php endif; ?>
-  </ul>
-  <ul class="nav navbar-nav navbar-right">
-     <?php if($this->session->userdata('logged_in')) : ?>
-                    <div class="btn-group" role="group" aria-label="Data baru">
-                        <li><?php echo anchor('user/logout', 'Logout', array('class' => 'glyphicon glyphicon-log-out')); ?></li>
-                    </div>
-      <?php endif; ?>
-  </ul>
-    </div>
+    <?php if($this->session->userdata('logged_in')) : ?>
+		<div class="row"></div>
+		<ul class="nav nav-pills nav-stacked">
+			<li class="active"><a href="<?php echo site_url('user/index')?>"><span class="glyphicon glyphicon-home"></span> Home</a></li>			
+			<li><a href="<?php echo site_url('user_jadwal/')?>"><span class="glyphicon glyphicon-list-alt"></span>  Jadwal</a></li>
+			<li><a href="<?php echo site_url('user_pencarian/')?>"><span class="glyphicon glyphicon-search"></span>  Pencarian</a></li>      				
+			<li><a href="<?php echo site_url('user/logout')?>"><span class="glyphicon glyphicon-log-out"></span>  Logout</a></li>			
+		</ul>
   </div>
-</nav>
+  <?php endif; ?>
+	<div class="col-md-10">
